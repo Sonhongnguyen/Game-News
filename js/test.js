ef1a507f9563c5a7e00c2bfe3ba30e38a6cc314d
sonnh <sonnh@mail.wgentech.com>
2023-02-28 10:31:04 +0700

    Fix style home page

---
+++ js/test.js
@@ -0,0 +1,13 @@
+//test
+// $(document).ready(function () {
+//     $(this).scroll(function(){
+//         var ScrollTop = $(this).scrollTop();
+  
+//         if (ScrollTop > 1) {
+//             $(test).css({ "background-color":"red", "margin-top":"0"});
+//         }else{
+//         $(test).css({ "background-color":"brown", "margin-top":"30px" });
+//         }
+//     });
+//   });
+  
